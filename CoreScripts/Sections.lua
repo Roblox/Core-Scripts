b251927afc883ca3fc4b195e3f1d22706d2a83e4
hockeychaos <ben@roblox.com>
2014-08-08 15:12:36 -0700

    Delete Sections.lua

--- CoreScripts/Sections.lua
+++
@@ -1,3 +0,0 @@
-settings().FastLogSettings:SetGroupEnable("Network", 1, true)
-settings().FastLogSettings:SetGroupEnable("MegaClusterNetwork", 1, true)
-settings().FastLogSettings:SetGroupEnable("MegaClusterNetworkInit", 1, true)
