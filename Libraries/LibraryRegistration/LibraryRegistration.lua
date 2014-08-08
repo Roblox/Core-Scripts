da31152696c997645ddd6c178e1bb20777d9a331
Ben <ben@roblox.com>
2014-08-08 15:15:39 -0700

    Remove unnecessary library registrations

--- Libraries/LibraryRegistration/LibraryRegistration.lua
+++ Libraries/LibraryRegistration/LibraryRegistration.lua
@@ -1,8 +1,6 @@
 -- Library Registration Script
 -- This script is used to register RbxLua libraries on game servers, so game scripts have
 -- access to all of the libraries (otherwise only local scripts do)
-
-local deepakTestingPlace = 3569749
 local sc = game:GetService("ScriptContext")
 local tries = 0
  
@@ -14,10 +12,6 @@ end
  
 if sc then
 	sc:RegisterLibrary("Libraries/RbxGui", "45284430")
-	sc:RegisterLibrary("Libraries/RbxGear", "45374389")
-	if game.PlaceId == deepakTestingPlace then
-		sc:RegisterLibrary("Libraries/RbxStatus", "52177566")
-	end
 	sc:RegisterLibrary("Libraries/RbxUtility", "60595411")
 	sc:RegisterLibrary("Libraries/RbxStamper", "73157242")
 	sc:LibraryRegistrationComplete()
