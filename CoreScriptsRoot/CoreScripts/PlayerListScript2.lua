6d7174cdaac6e5e0b17116bee4a0dd61f77e2e1d
jmargh <jroth@roblox.com>
2015-01-07 17:15:02 -0800

    Fixing something with last commit

--- CoreScriptsRoot/CoreScripts/PlayerListScript2.lua
+++ CoreScriptsRoot/CoreScripts/PlayerListScript2.lua
@@ -779,13 +779,6 @@ local function getFriendCount(userId)
 	return friendCount
 end
 
-local ws, er = pcall(function()
-	local str = tostring(Player.userId)
-	local str1 = tostring(31318592)
-	local yaman = HttpRbxApiService:GetAsync('user/following-exists?userId='..str1..'&followerUserId='..str)
-	print(yaman)
-end)
-
 -- checks for both local player and other player
 local function checkForMaxFriends(otherPlayer)
 	local myFriendCount = getFriendCount()
