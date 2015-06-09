a2cb5aeae55c38a0fdfe8e45992323dd99cd4cdf
V D'Amelio <vdamelio@roblox.com>
2015-06-09 16:16:22 -0700

    Revert "Update MouseLock Icon Pos"
    This reverts commit 7afed564f0670bb55f56423728df3ffb73d2a053.

--- CoreScriptsRoot/LoadingScript.lua
+++ CoreScriptsRoot/LoadingScript.lua
@@ -461,7 +461,6 @@ renderSteppedConnection = Game:GetService("RunService").RenderStepped:connect(fu
 end)
 
 guiService.ErrorMessageChanged:connect(function()
-	print("ErrorMessageChanged")
 	if guiService:GetErrorMessage() ~= '' then
 		currScreenGui.ErrorFrame.ErrorText.Text = guiService:GetErrorMessage()
 		currScreenGui.ErrorFrame.Visible = true
