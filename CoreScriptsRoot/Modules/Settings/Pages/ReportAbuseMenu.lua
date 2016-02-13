1d86cf164ca5e401bd97fa758e21ae8012b29ebe
coolestkid <coolestkid@users.noreply.github.com>
2016-02-13 07:18:51 -0500

    Fix service references
    Use game:GetService to prevent breaking when services are renamed.

--- CoreScriptsRoot/Modules/Settings/Pages/ReportAbuseMenu.lua
+++ CoreScriptsRoot/Modules/Settings/Pages/ReportAbuseMenu.lua
@@ -9,6 +9,7 @@
 local CoreGui = game:GetService("CoreGui")
 local RobloxGui = CoreGui:WaitForChild("RobloxGui")
 local GuiService = game:GetService("GuiService")
+local Players = game:GetService("Players")
 
 ----------- UTILITIES --------------
 local utility = require(RobloxGui.Modules.Settings.Utility)
@@ -57,13 +58,13 @@ local function Initialize()
 
 	function this:UpdatePlayerDropDown()
 		playerNames = {}
-	    nameToRbxPlayer = {}
+	    	nameToRbxPlayer = {}
 
-		local players = game.Players:GetChildren()
+		local players = Players:GetPlayers()
 		local index = 1
 		for i = 1, #players do
 			local player = players[i]
-			if player:IsA('Player') and player ~= game:GetService("Players").LocalPlayer and player.UserId > 0 then
+			if player ~= Players.LocalPlayer and player.UserId > 0 then
 				playerNames[index] = player.Name
 				nameToRbxPlayer[player.Name] = player
 				index = index + 1
