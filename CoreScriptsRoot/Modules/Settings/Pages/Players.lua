335a2d0863a5e047d96b9fc468915dc001f693f9
3tianne <3tianne@gmail.com>
2016-04-16 00:58:52 +0100

    Convert userId to a string instead of using math.max()
    math.max(1, player.userId) just returns the player's ID as an int - use
    tostring() to convert it to a string.

--- CoreScriptsRoot/Modules/Settings/Pages/Players.lua
+++ CoreScriptsRoot/Modules/Settings/Pages/Players.lua
@@ -274,7 +274,7 @@ local function Initialize()
 					table.insert(existingPlayerLabels, index, frame)
 				end
 				frame.Name = 'PlayerLabel'..player.Name
-				frame.Icon.Image = 'http://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userId='..math.max(1, player.userId)
+				frame.Icon.Image = 'http://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userId='..tostring(player.userId)
 				frame.NameLabel.Text = player.Name
 				frame.ImageTransparency = frameDefaultTransparency
 
