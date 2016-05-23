edec9e052ff5ab92976daea760790304f0683a07
Ben <ben@roblox.com>
2016-05-23 10:25:30 -0700

    merge in UWP branch fixes

--- CoreScriptsRoot/Modules/Settings/Pages/GameSettings.lua
+++ CoreScriptsRoot/Modules/Settings/Pages/GameSettings.lua
@@ -586,7 +586,7 @@ local function Initialize()
 
 	createVolumeOptions()
 
-	if platform == Enum.Platform.Windows or platform == Enum.Platform.OSX then
+	if platform == Enum.Platform.Windows or platform == Enum.Platform.UWP or platform == Enum.Platform.OSX then
 		createGraphicsOptions()
 	end
 
