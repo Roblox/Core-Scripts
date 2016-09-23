5ddd7f7a3ea6a4903d815858e09438cf5f4d0363
Doug Banks <doug.banks@gmail.com>
2016-09-23 13:31:43 -0700

    changed indentation

--- CoreScriptsRoot/Modules/StyleWidgets.lua
+++ CoreScriptsRoot/Modules/StyleWidgets.lua
@@ -20,19 +20,19 @@ local StyleWidgets = {}
 StyleWidgets.TabSelectionHeight = 6
 
 function StyleWidgets.MakeTabSelectionWidget(parent)
-	local tabSelection = utility:Create'ImageLabel'
-	{
-		Name = "TabSelection",
-		Image = "rbxasset://textures/ui/Settings/MenuBarAssets/MenuSelection.png",
-		ScaleType = Enum.ScaleType.Slice,
-		SliceCenter = Rect.new(3,1,4,5),
-		Visible = false,
-		BackgroundTransparency = 1,
-		Size = UDim2.new(1,0,0,StyleWidgets.TabSelectionHeight ),
-		Position = UDim2.new(0,0,1,-StyleWidgets.TabSelectionHeight ),
-		Parent = parent
-	};
-  
+  local tabSelection = utility:Create'ImageLabel'
+  {
+    Name = "TabSelection",
+    Image = "rbxasset://textures/ui/Settings/MenuBarAssets/MenuSelection.png",
+    ScaleType = Enum.ScaleType.Slice,
+    SliceCenter = Rect.new(3,1,4,5),
+    Visible = false,
+    BackgroundTransparency = 1,
+    Size = UDim2.new(1,0,0,StyleWidgets.TabSelectionHeight ),
+    Position = UDim2.new(0,0,1,-StyleWidgets.TabSelectionHeight ),
+    Parent = parent
+  };
+
   return tabSelection
 end
 
