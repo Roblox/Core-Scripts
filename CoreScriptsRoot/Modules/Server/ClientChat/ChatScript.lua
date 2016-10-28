8eafc1fd748eef7208309cb79d0be51b7d665881
ConorGriffin37 <conor.griffin37@mail.dcu.ie>
2016-10-28 14:54:01 -0700

    Fix broken fix

--- CoreScriptsRoot/Modules/Server/ClientChat/ChatScript.lua
+++ CoreScriptsRoot/Modules/Server/ClientChat/ChatScript.lua
@@ -3,6 +3,7 @@
 --	// Description: Hooks main chat module up to Topbar in corescripts.
 
 local StarterGui = game:GetService("StarterGui")
+local GuiService = game:GetService("GuiService")
 local MAX_COREGUI_CONNECTION_ATTEMPTS = 10
 
 local function DoEverything()
