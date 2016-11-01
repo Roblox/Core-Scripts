e484ccf0cb72b5aaa39c97649ca6d770a3c06fd7
DarraghGriffin <darragh.griffin24@mail.dcu.ie>
2016-10-31 17:32:44 -0700

    Change Create implementations to set parent last

--- CoreScriptsRoot/Modules/Settings/Utility.lua
+++ CoreScriptsRoot/Modules/Settings/Utility.lua
@@ -49,12 +49,18 @@ do
 	function Util.Create(instanceType)
 		return function(data)
 			local obj = Instance.new(instanceType)
+			local parent = nil
 			for k, v in pairs(data) do
 				if type(k) == 'number' then
 					v.Parent = obj
+				elseif k == 'Parent' then
+					parent = v
 				else
 					obj[k] = v
 				end
+			end
+			if parent then
+				obj.Parent = parent
 			end
 			return obj
 		end
