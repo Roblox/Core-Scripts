231e87a2fe7a978ea576a57a8a1510816b6200b8
ConorGriffin37 <conor.griffin37@mail.dcu.ie>
2017-01-16 13:51:50 -0800

    Add comment about why 0, 0 and 1, 1 viewport checks

--- CoreScriptsRoot/LoadingScript.lua
+++ CoreScriptsRoot/LoadingScript.lua
@@ -25,6 +25,8 @@ local function getViewportSize()
 		game.Workspace.Changed:wait()
 	end
 
+	-- ViewportSize is initally set to 1, 1 in Camera.cpp constructor.
+	-- Also check against 0, 0 incase this is changed in the future.
 	while game.Workspace.CurrentCamera.ViewportSize == Vector2.new(0,0) or
 		game.Workspace.CurrentCamera.ViewportSize == Vector2.new(1,1) do
 		game.Workspace.CurrentCamera.Changed:wait()
