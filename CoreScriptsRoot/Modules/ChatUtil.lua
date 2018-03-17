265456b2db1004243fdab6fb1cfe5ffb5a341321
stillunt1tled <dr.freeman.forever@gmail.com>
2018-03-17 08:33:30 -0400

    ChatUtil: Deprecate lower camel case method names
    Resolves issue #1043 by renaming the methods to reflect the method names of BindableEvent.
    
    Signed-off-by: stillunt1tled <dr.freeman.forever@gmail.com>

--- CoreScriptsRoot/Modules/ChatUtil.lua
+++ CoreScriptsRoot/Modules/ChatUtil.lua
@@ -7,24 +7,39 @@ do
 
 		local mArgData = nil
 		local mArgDataCount = nil
-
-		function sig:fire(...)
+		
+		function sig:Fire(...)
 			mArgData = {...}
 			mArgDataCount = select('#', ...)
 			mSignaler:Fire()
 		end
-
-		function sig:connect(f)
+		
+		-- deprecated
+		function sig:fire(...)
+			self:Fire(...)
+		end
+		
+		function sig:Connect(f)
 			if not f then error("connect(nil)", 2) end
 			return mSignaler.Event:connect(function()
 				f(unpack(mArgData, 1, mArgDataCount))
 			end)
 		end
-
-		function sig:wait()
+		
+		-- deprecated
+		function sig:connect(f)
+			self:Connect(f)
+		end
+		
+		function sig:Wait()
 			mSignaler.Event:wait()
 			assert(mArgData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
 			return unpack(mArgData, 1, mArgDataCount)
+		end
+		
+		-- deprecated
+		function sig:wait()
+			sig:Wait();
 		end
 
 		return sig
